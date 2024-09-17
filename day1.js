1e33362d9e3953b884a0420590eaa49c1ca52564
Amarnath Manchimsetti <150233430+satyamarmm@users.noreply.github.com>
2024-09-17 18:24:02 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,16 @@
+// a="hema"
+// b="veera"
+// c="venkata"
+// d="satya"
+// e="amar"
+// console.log(a)
+
+document.write("amarnath");
+
+console.log(4646); 
+console.log("amar");
+
+a="satya"
+b=02;
+console.log(a);
+console.log(b);
